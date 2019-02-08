79982702edb2285020bb6b4add24864ddb15fd5b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2019-02-08 08:03:53 +0300

    build(gradle): fix kotlin buildSrc
    Minor fixes to variable declarations

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,7 +1,7 @@
 object Config {
-    serviceAccountEmail = getChatBotServiceAccountKey()
-    fabricApiKey = getFabricApiKey()
-    rollbarAccessToken = getRollbarAccessToken()
+//    serviceAccountEmail = getChatBotServiceAccountKey()
+//    fabricApiKey = getFabricApiKey()
+//    rollbarAccessToken = getRollbarAccessToken()
 
     /**
      * Set this to true to create two separate APKs instead of one:
@@ -17,34 +17,33 @@ object Config {
     const val enableUniversalApk = true
 
     // ABI codes
-    val abiCodes = hashMapOf("armeabi-v7a": 1, "x86": 2, "mips": 3, "x86_64": 4, "armeabi": 5, "arm64-v8a": 6)
+//    val abiCodes = hashMapOf("armeabi-v7a": 1, "x86": 2, "mips": 3, "x86_64": 4, "armeabi": 5, "arm64-v8a": 6)
 
     // For per-density APKs, create a similar map like this:
-    densityCodes = ["ldpi": 1, 'mdpi': 2, 'hdpi': 3, 'xhdpi': 4, "xxhdpi": 5, "xxxhdpi": 6]
+//    densityCodes = ["ldpi": 1, 'mdpi': 2, 'hdpi': 3, 'xhdpi': 4, "xxhdpi": 5, "xxxhdpi": 6]
 
     // -------------------- DEPENDENCIES --------------------
 
     // support lib
-    supportLibVersion = "27.0.2"
+    val supportLibVersion = "27.0.2"
 
     // multidex
-    multiDexEnabledVersion = "1.0.2"
+    val multiDexEnabledVersion = "1.0.2"
 
     // Dependency Injection
-    javaxAnnotationVersion = "1.0"
-    daggerVersion = "2.11"
-    javaxAnnotationVersion = "1.0"
-    javaxInjectVersion = "1"
+    val javaxAnnotationVersion = "1.0"
+    val daggerVersion = "2.11"
+    val javaxInjectVersion = "1"
 
     // -------------------- TEST DEPENDENCIES -----------------------
-    jUnitVersion = "4.12"
-    espressoVersion = "3.0.1"
-    truthVersion = "0.35"
-    mockitoVersion = "2.7.1"
-    robolectricVersion = "3.3.2"
-    testRunnerVersion = "1.0.1"
-    espressoContribVersion = "2.0"
-    assertJVersion = "3.6.2"
-    mockitoKotlinVersion = "1.4.0"
-    hamcrestVersion = "1.3"
+    val jUnitVersion = "4.12"
+    val espressoVersion = "3.0.1"
+    val truthVersion = "0.35"
+    val mockitoVersion = "2.7.1"
+    val robolectricVersion = "3.3.2"
+    val testRunnerVersion = "1.0.1"
+    val espressoContribVersion = "2.0"
+    val assertJVersion = "3.6.2"
+    val mockitoKotlinVersion = "1.4.0"
+    val hamcrestVersion = "1.3"
 }
